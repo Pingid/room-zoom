e3aa725059a9e98ab9dcc9d212d68a51fce3317b
Student <student@m-b2409.arts.local>
2017-06-02 16:08:53 +0100

    Added tube-zoom project

---
+++ tube-zoom/Assets/scripts/playerController.js
@@ -0,0 +1,15 @@
+﻿#pragma strict
+
+public var upperLimmit : float;
+
+function Start () {
+
+}
+
+function Update () {
+	if (Input.mousePosition.y > 0) {
+		var pos : float = (Input.mousePosition.y / Screen.height) * upperLimmit;
+
+		transform.position.z = pos;
+	}
+}
